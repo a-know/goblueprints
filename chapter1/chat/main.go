0f47ce9bbbcc26deb2b87dcd1f0979c7feef86e2
a-know <a.know.3373@gmail.com>
2017-07-16 07:15:21 +0900

    Using text/template

--- chapter1/chat/main.go
+++ chapter1/chat/main.go
@@ -3,17 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-      <html>
-        <head><title>Go Blueprints Chat</title></head>
-        <body>Let's chatting!</body>
-      </html>
-    `))
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// Handling HTTP Request
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 	// Starting web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
