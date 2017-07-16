2635c98cfe5e9a3a929e480b4da62b58d83705f9
a-know <a.know.3373@gmail.com>
2017-07-16 18:08:30 +0900

    Enable to specify port number by args

--- chapter1/chat/main.go
+++ chapter1/chat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"path/filepath"
@@ -19,17 +20,20 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "port number")
+	flag.Parse()
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	// Starting chatroom
 	go r.run()
 	// Starting web server
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Starting Web server... port : ", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
