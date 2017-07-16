745f469a6053a6482bce7bac11ad840bb5e46247
a-know <a.know.3373@gmail.com>
2017-07-16 16:15:37 +0900

    Define models for chatroom

--- chapter1/chat/room.go
+++ chapter1/chat/room.go
@@ -3,4 +3,10 @@ package main
 type room struct {
 	// channel for save message sending other
 	forward chan []byte
+	// channel for client intend to join room. use for add client to `clients`
+	join chan *client
+	// channel for client intend to leave room. use for remove client from `clients`
+	leave chan *client
+	// save all joined clients
+	clients map[*client]bool
 }
