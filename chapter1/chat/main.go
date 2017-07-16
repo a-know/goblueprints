887b1b862a5e52a8481f35f3506e14582db375c3
a-know <a.know.3373@gmail.com>
2017-07-16 16:52:44 +0900

    Starting chatroom

--- chapter1/chat/main.go
+++ chapter1/chat/main.go
@@ -23,7 +23,11 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	// Starting chatroom
+	go r.run()
 	// Starting web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
