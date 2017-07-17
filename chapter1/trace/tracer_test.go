f3a3e595b99f31c6bf61d882c577569771d0c080
a-know <a.know.3373@gmail.com>
2017-07-17 12:30:37 +0900

    Implement temporary tests

---
+++ chapter1/trace/tracer_test.go
@@ -0,0 +1,19 @@
+package trace
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("Return value from New is nil.")
+	} else {
+		tracer.Trace("Hello, trace package!")
+		if buf.String() != "Hello, trace package!\n" {
+			t.Errorf("It get '%s' output is wrong.", buf.String())
+		}
+	}
+}
