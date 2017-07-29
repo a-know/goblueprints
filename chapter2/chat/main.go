c4fc152e8e755a973c58591432615b04fa5ca336
a-know <a.know.3373@gmail.com>
2017-07-29 13:02:01 +0900

    Display username

--- chapter2/chat/main.go
+++ chapter2/chat/main.go
@@ -13,6 +13,7 @@ import (
 	"github.com/stretchr/gomniauth/providers/facebook"
 	"github.com/stretchr/gomniauth/providers/github"
 	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 )
 
 type templateHandler struct {
@@ -26,7 +27,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, r)
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCoockie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCoockie.Value)
+	}
+	t.templ.Execute(w, data)
 }
 
 func main() {
