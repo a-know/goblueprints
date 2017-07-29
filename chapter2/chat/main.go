fea623ea2328219685bf3c410080bd6040ecf3f2
a-know <a.know.3373@gmail.com>
2017-07-29 11:02:33 +0900

    Implement login process
    - use gomniauth
    - Treat credentials as environment values
    - use direnv treat anvironment values

--- chapter2/chat/main.go
+++ chapter2/chat/main.go
@@ -4,9 +4,15 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
 	"text/template"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
 )
 
 type templateHandler struct {
@@ -27,6 +33,13 @@ func main() {
 	var addr = flag.String("addr", ":8080", "port number")
 	var logging = flag.Bool("logging", true, "logging with stdout")
 	flag.Parse()
+	// setup gomniauth
+	gomniauth.SetSecurityKey(os.Getenv("SECURITY_KEY"))
+	gomniauth.WithProviders(
+		facebook.New(os.Getenv("FACEBOOK_CLIENT_ID"), os.Getenv("FACEBOOK_SECRET_KEY"), "http://localhost:8080/auth/callback/facebook"),
+		github.New(os.Getenv("GITHUB_CLIENT_ID"), os.Getenv("GITHUB_SECRET_KEY"), "http://localhost:8080/auth/callback/github"),
+		google.New(os.Getenv("GOOGLE_CLIENT_ID"), os.Getenv("GOOGLE_SECRET_KEY"), "http://localhost:8080/auth/callback/google"),
+	)
 	r := newRoom(*logging)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
