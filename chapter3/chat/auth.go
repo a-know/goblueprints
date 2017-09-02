4d0774618604d620d12633c4a7e2509707b5a1da
a-know <a.know.3373@gmail.com>
2017-09-02 10:23:23 +0900

    $ cp -r chapter2 chapter3

---
+++ chapter3/chat/auth.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/objx"
+)
+
+type authHandler struct {
+	next http.Handler
+}
+
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := r.Cookie("auth"); err == http.ErrNoCookie {
+		// not authenticated yet
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	} else if err != nil {
+		// something wrong
+		panic(err.Error())
+	} else {
+		// successful, call next wrapped handler
+		h.next.ServeHTTP(w, r)
+	}
+}
+
+func MustAuth(handler http.Handler) http.Handler {
+	return &authHandler{next: handler}
+}
+
+// Path style : /auth/{action}/{provider}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := strings.Split(r.URL.Path, "/")
+	if len(segs) == 4 {
+		action := segs[2]
+		provider := segs[3]
+		switch action {
+		case "login":
+			provider, err := gomniauth.Provider(provider)
+			if err != nil {
+				log.Fatalln("Failed to get auth provider:", provider, "-", err)
+			}
+			loginURL, err := provider.GetBeginAuthURL(nil, nil)
+			if err != nil {
+				log.Fatalln("Error occurs in calling GetBeginAuthURL:", provider, "-", err)
+			}
+			w.Header().Set("Location", loginURL)
+			w.WriteHeader(http.StatusTemporaryRedirect)
+		case "callback":
+			provider, err := gomniauth.Provider(provider)
+			if err != nil {
+				log.Fatalln("Failed to get provider:", provider, "-", err)
+			}
+			creds, err := provider.CompleteAuth(objx.MustFromURLQuery(r.URL.RawQuery))
+			if err != nil {
+				log.Fatalln("Could not finish authentication:", provider, "-", err)
+			}
+			user, err := provider.GetUser(creds)
+			if err != nil {
+				log.Fatalln("Failed to get user data:", provider, "-", err)
+			}
+			authCookieValue := objx.New(map[string]interface{}{
+				"name": user.Name(),
+			}).MustBase64()
+			http.SetCookie(w, &http.Cookie{
+				Name:  "auth",
+				Value: authCookieValue,
+				Path:  "/"})
+			w.Header()["Location"] = []string{"/chat"}
+			w.WriteHeader(http.StatusTemporaryRedirect)
+		default:
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprintf(w, "Not supported action: %s", action)
+		}
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "Not supported request: %s", r.URL.Path)
+	}
+}
---
+++ chapter3/chat/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+	"text/template"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// Handling HTTP Request
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCoockie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCoockie.Value)
+	}
+	t.templ.Execute(w, data)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8080", "port number")
+	var logging = flag.Bool("logging", true, "logging with stdout")
+	flag.Parse()
+	// setup gomniauth
+	gomniauth.SetSecurityKey(os.Getenv("SECURITY_KEY"))
+	gomniauth.WithProviders(
+		facebook.New(os.Getenv("FACEBOOK_CLIENT_ID"), os.Getenv("FACEBOOK_SECRET_KEY"), "http://localhost:8080/auth/callback/facebook"),
+		github.New(os.Getenv("GITHUB_CLIENT_ID"), os.Getenv("GITHUB_SECRET_KEY"), "http://localhost:8080/auth/callback/github"),
+		google.New(os.Getenv("GOOGLE_CLIENT_ID"), os.Getenv("GOOGLE_SECRET_KEY"), "http://localhost:8080/auth/callback/google"),
+	)
+	r := newRoom(*logging)
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
+	http.Handle("/room", r) // for WebSocket connection endpoint
+	// Starting chatroom
+	go r.run()
+	// Starting web server
+	log.Println("Starting Web server... port : ", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
