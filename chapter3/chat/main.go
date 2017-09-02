f85c915f540f68b1929cd4ae0e3b466c0b521d51
a-know <a.know.3373@gmail.com>
2017-09-02 10:50:32 +0900

    Implement logout

--- chapter3/chat/main.go
+++ chapter3/chat/main.go
@@ -50,6 +50,16 @@ func main() {
 	r := newRoom(*logging)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name:   "auth",
+			Value:  "",
+			Path:   "/",
+			MaxAge: -1,
+		})
+		w.Header()["Location"] = []string{"/chat"}
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	})
 	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r) // for WebSocket connection endpoint
 	// Starting chatroom
