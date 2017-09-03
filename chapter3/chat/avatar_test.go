2f75bcf08da2805cd2adea6888fe9fa21be8bf3c
a-know <a.know.3373@gmail.com>
2017-09-03 21:55:00 +0900

    GravatarAvatar

--- chapter3/chat/avatar_test.go
+++ chapter3/chat/avatar_test.go
@@ -26,3 +26,16 @@ func TestAuthAvatar(t *testing.T) {
 		}
 	}
 }
+
+func TestGravatarAvatar(t *testing.T) {
+	var gravatarAvatar GravatarAvatar
+	client := new(client)
+	client.userData = map[string]interface{}{"email": "MyEmailAddress@example.com"}
+	url, err := gravatarAvatar.GetAvatarURL(client)
+	if err != nil {
+		t.Error("GravatarAvatar.GetAvatarURL should not return error.")
+	}
+	if url != "//www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346" {
+		t.Errorf("GravatarAvatar.GetAvatarURL returns wrong value :%s", url)
+	}
+}
